a85a0928bcb5ff4b6a1060244e8788d7574ded94
Samkok <hengsamkok76@gmail.com>
2025-06-26 20:07:20 +0700

    Updated _layout.tsx

--- app/(app)/(tabs)/inventory/_layout.tsx
+++ app/(app)/(tabs)/inventory/_layout.tsx
@@ -4,7 +4,7 @@ import { Stack } from 'expo-router';
 export default function InventoryLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen key="index" name="index" />
+      <Stack.Screen key="index-inventory" name="index" />
       <Stack.Screen key="low-stock" name="low-stock" />
     </Stack>
   );
