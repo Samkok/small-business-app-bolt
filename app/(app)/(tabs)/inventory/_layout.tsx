f9352dc6538ea41292ca0308236c93a7dce0df7c
Samkok <hengsamkok76@gmail.com>
2025-07-03 14:32:24 +0700

    Fix inventory layout screen declarations

--- app/(app)/(tabs)/inventory/_layout.tsx
+++ app/(app)/(tabs)/inventory/_layout.tsx
@@ -4,10 +4,8 @@ import { Stack } from 'expo-router';
 export default function InventoryLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen key="index-inventory" name="index" />
-      <Stack.Screen key="low-stock" name="low-stock" />
-      <Stack.Screen key="product-selection" name="product-selection" />
-      <Stack.Screen key="import-form" name="import-form" />
+      <Stack.Screen name="index" />
+      <Stack.Screen name="low-stock" />
     </Stack>
   );
 }
