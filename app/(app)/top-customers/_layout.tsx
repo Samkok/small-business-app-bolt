08a097c635731b8695102a6dafcb108427bbc035
Samkok <hengsamkok76@gmail.com>
2025-09-26 17:52:19 +0700

    Fix duplicate customers screen error by removing conflicting directory

---
+++ app/(app)/top-customers/_layout.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Stack } from 'expo-router';
+
+export default function TopCustomersLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="customer-sales-report" />
+    </Stack>
+  );
+}
