88d414f65731d579829059da672a6f316b4f8a86
Samkok <hengsamkok76@gmail.com>
2025-09-26 17:56:12 +0700

    Deleted customer-sales-report.tsx

--- app/(app)/(tabs)/topCustomers/index.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { Redirect } from 'expo-router';
-
-export default function TopCustomersIndex() {
-  // Redirect to the customer sales report as the default view
-  return <Redirect href="/(app)/(tabs)/top-customers/customer-sales-report" />;
-}
