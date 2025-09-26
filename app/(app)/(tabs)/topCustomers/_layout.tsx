de065816e7fb0eb8f78a698bd4fb997c7a88033d
Samkok <hengsamkok76@gmail.com>
2025-09-26 17:33:49 +0700

    Updated _layout.tsx

--- app/(app)/(tabs)/topCustomers/_layout.tsx
+++ app/(app)/(tabs)/topCustomers/_layout.tsx
@@ -1,11 +1,35 @@
+import { Tabs } from 'expo-router';
 import React from 'react';
-import { Stack } from 'expo-router';
 
-export default function TopCustomersLayout() {
+import { TabBarIcon } from '@/src/components/navigation/TabBarIcon';
+import { useColorScheme } from '@/src/hooks/useColorScheme';
+import { tabBarStyle } from '@/src/styles/tabBarStyle';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
   return (
-    <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="overview" />
-      <Stack.Screen name="customer-sales-report" />
-    </Stack>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle,
+      }}
+    >
+      <Tabs.Screen
+        name="customers"
+        options={{
+          href: null,
+        }}
+      />
+      <Tabs.Screen
+        name="top-customers"
+        options={{
+          title: 'Top Customers',
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="users" color={color} />
+          ),
+        }}
+      />
+    </Tabs>
   );
 }
